b58a25d3ff86bb0b2a4133138b112e1a99cd12a9
Victor Ximenes Paixão <victor_paixao1991@hotmail.com>
2025-02-19 15:46:34 +0000

    update file

--- csv_generator-overview-nfts.py
+++ csv_generator-overview-nfts.py
@@ -7,13 +7,13 @@ import os
 import re
 
 '''
-Python file responsible for collect overview information about collection and transform to CSV file for Power BI project
+Python file responsible for collect overview information about NFTs and transform to CSV file for Power BI project
 '''
 
 load_dotenv()
 
-slug_collection = 'boredape-baseclub'
-initial_token = 'LXBrPTE4ODI3MTg3NTQ='
+#'boredapeyachtclub'
+slug_collection = 'boredapeyachtclub'
 
 url = f'https://api.opensea.io/api/v2/collection/{slug_collection}/nfts?limit=200'
 api_key = os.getenv('api_key')
@@ -35,4 +35,5 @@ df_boredape_baseclub = pd.concat([df_start,df_boredape_baseclub])
 df_boredape_baseclub['identifier'] = df_boredape_baseclub['identifier'].astype(str)
 df_boredape_baseclub.sort_values(by=['identifier'], ascending=False)
 
-df_boredape_baseclub.to_csv("boredape_baseclue_overview_datasourcePBI.csv", index=False)
+df_boredape_baseclub.to_csv(f"{slug_collection}_overview_datasourcePBI.csv", index=False)
+
